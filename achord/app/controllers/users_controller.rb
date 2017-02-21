f3e5aaab58cb094515b038b74bcda39c88ea0b99
Josh Trotter <jtrot1000@gmail.com>
2017-02-20 22:28:42 -0800

    add UsersController#show + #index

--- achord/app/controllers/users_controller.rb
+++ achord/app/controllers/users_controller.rb
@@ -12,8 +12,14 @@ class UsersController < ApplicationController
     end
   end
 
-  def new
-    @user = User.new
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def index
+    @users = User.all
+    render :index
   end
 
   private
